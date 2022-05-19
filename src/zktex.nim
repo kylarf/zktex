ee2ed37cb9ace807a8df99cdb02008566546930c
kylarf <56093925+kylarf@users.noreply.github.com>
2022-05-19 00:35:02 -0700

    Add note creation and hash storage functionality

--- src/zktex.nim
+++ src/zktex.nim
@@ -1,5 +1,46 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import std/[os, json, strformat, sha1]
+import zktex/new
+
+let
+  zkdir = expandTilde("~/.zktex")
+  hashedNotesPath = fmt"{zkdir}/hashes.json"
+
+
+proc usage() =
+  echo "TODO: Print usage to console."
+
+
+proc loadHashes(): JsonNode =
+  try:
+    return parseJson(readFile(hashedNotesPath))
+  except IOError:
+    echo "Creating JSON hash file"
+    writeFile(hashedNotesPath, "{}")
+    return parseJson(readFile(hashedNotesPath))
+
+
+proc saveHashes(noteHashes: JsonNode) =
+  writeFile(hashedNotesPath, pretty(noteHashes))
+
+
+proc main() =
+  let noteHashes = loadHashes()
+
+  if paramCount() < 1:
+    usage()
+  else:
+    discard existsOrCreateDir(zkdir)
+    let args = commandLineParams()[1..^1]
+
+    case paramStr(1):
+      of "new":
+        let (noteID, noteHash) = newNote(args)
+        noteHashes[noteID] = % $noteHash
+      else:
+        usage()
+
+    saveHashes(noteHashes)
+
 
 when isMainModule:
-  echo("Hello, World!")
+  main()
---
+++ src/zktex/new.nim
@@ -0,0 +1,19 @@
+import std/[os, times, sha1, strformat, osproc]
+
+proc genID(): string =
+  getTime().format("yyyyMMddmm")
+
+proc newNote*(args: seq[string]): (string, SecureHash) =
+  let
+    noteID = genID()
+    noteTemplate = "Test note template"
+    notePath = expandTilde(fmt"~/.zktex/{noteID}.tex")
+
+  writeFile(notePath, noteTemplate)
+
+  discard execCmd(fmt"nvim {notePath}")
+  let noteHash = secureHashFile(notePath)
+
+  return (noteID, noteHash)
+
+
