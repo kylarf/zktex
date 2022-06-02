dc5d3890de114387d9522c8ee3de70ba21c5a20a
kylarf <56093925+kylarf@users.noreply.github.com>
2022-06-02 00:23:14 -0700

    Add sort, display for note search results; general improvements to note-finding

--- src/zktex/find.nim
+++ src/zktex/find.nim
@@ -1,18 +1,20 @@
 import
-  std/[os, strutils, editdistance, sequtils, sugar, re, tables, stats],
+  std/[os, strutils, editdistance, sequtils, sugar, re, stats, strformat,
+       algorithm],
   ./config
 
-const NonAlphaNum* = AllChars - Digits - Letters
+const
+  NonAlphaNum* = AllChars - Digits - Letters
+  noteFilePattern = "[0-9]*.tex"
 
 
 type
   SearchResult* = object
+    id*: string
     title*: string
     score*: float
 
-
-func levenshteinRatio*(s1, s2: string): float =
-  1 - editDistance(s1, s2) / max(s1.len, s2.len)
+  SearchResults* = seq[SearchResult]
 
 
 func splitKeywords*(text: string): seq[string] =
@@ -30,27 +32,40 @@ func getTitle*(noteText: string): string =
     return noteText[initial .. final]
 
 
+func levenshteinRatio*(s1, s2: string): float =
+  1 - editDistance(s1, s2) / max(s1.len, s2.len)
+
+
 func fuzzyMatchWords*(words: seq[string], keyword: string): float =
   max(words.map(word => levenshteinRatio(word, keyword)))
 
 
-proc findKeywords*(keywords, args: seq[string], settings: ZkConfig):
-                   TableRef[string, SearchResult] =
-  setCurrentDir(settings["zkdir"].expandTilde)
+func matchScore*(noteWords, keywords: seq[string]): float =
+  mean(keywords.map(kw => noteWords.fuzzyMatchWords(kw)))
 
-  let
-    noteFilePattern = "[0-9]*.tex"
-  var
-    noteText, noteID, title: string
-    noteWords: seq[string]
-    score: float
-  result = newTable[string, SearchResult]()
 
-  for note in walkFiles(noteFilePattern):
-    noteText = readFile(note)
-    noteWords = noteText.splitKeywords()
-    score = mean(keywords.map(kw => noteWords.fuzzyMatchWords(kw)))
-    if score >= 0.8:
-      noteID = splitFile(note).name
-      title = noteText.getTitle()
-      result[noteID] = SearchResult(title: title, score: score)
+proc findKeywords*(keywords: seq[string], path: string): SearchResults =
+  setCurrentDir(path)
+  result = collect:
+    for note in walkFiles(noteFilePattern):
+      let
+        noteText = readFile(note)
+        noteWords = noteText.splitKeywords()
+        score = noteWords.matchScore(keywords)
+      if score >= 0.8:
+        let
+          noteID = splitFile(note).name
+          title = noteText.getTitle()
+        SearchResult(id: noteID, title: title, score: score)
+
+
+proc sortResults*(results: var SearchResults) =
+  results.sort do (a, b: SearchResult) -> int:
+    cmp(a.score, b.score)
+
+
+proc show*(results: SearchResults, numbered: bool = false) =
+  for i, res in results.pairs:
+    if numbered:
+      stdout.write(&"{i}.\t")
+    stdout.writeLine(&"{res.id} | {res.title}")
